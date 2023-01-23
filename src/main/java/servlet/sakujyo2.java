955ad23edc69ac06665c7bba111ddf17dcbd66e6
sirogane <y.shirogane.sys22@morijyobi.ac.jp>
2023-01-23 11:00:55 +0900

    削除機能を追加

---
+++ src/main/java/servlet/sakujyo2.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.StudentDAO;
+
+/**
+ * Servlet implementation class sakujyo2
+ */
+@WebServlet("/sakujyo2")
+public class sakujyo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public sakujyo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		
+		int result =StudentDAO.DeleteConnection(name);
+	
+		if(result==1) {
+			String view = "WEB-INF/view/SakujyoSuccess.jsp";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			dispatcher.forward(request, response);
+		}else {
+			String view = "WEB-INF/view/SakujyoFail.jsp";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
